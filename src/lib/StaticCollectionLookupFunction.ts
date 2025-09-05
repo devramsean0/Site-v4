e45ec10150cfa802cff2934a485aa7fb2178cf48
Sean Outram <me@sean.cyou>
2025-09-05 08:52:07 +0000

    A very rusty restart

--- src/lib/StaticCollectionLookupFunction.ts
+++
@@ -1,3 +0,0 @@
-export function StaticCollectionLookupF(collection: any[], lookupValue: string, lookupKey: string = "id", resultKey: string) {
-    return collection.find((data: any) => data[lookupKey] === lookupValue).data[resultKey]
-}
