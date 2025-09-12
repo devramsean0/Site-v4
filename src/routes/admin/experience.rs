904136b1457f3c35c25d469828d589b717b8aa8e
Sean Outram <me@sean.cyou>
2025-09-12 10:52:44 +0000

    Implement somewhat ergonomic auth

--- src/routes/admin/experience.rs
+++ src/routes/admin/experience.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use crate::{db, templates::AdminExperienceListTemplate};
+use crate::{db, templates::AdminExperienceListTemplate, utils};
 use actix_session::Session;
 use actix_web::{
     get,
@@ -10,29 +10,17 @@ use actix_web::{
 use askama::Template;
 use async_sqlite::Pool;
 
-#[get("/admin/experience")]
+#[get("/experience")]
 pub async fn experience_list(
     request: HttpRequest,
     db_pool: web::Data<Arc<Pool>>,
     session: Session,
 ) -> HttpResponse {
-    let session_id = match session.get::<String>("session_id") {
-        Ok(Some(id)) => id,
-        _ => {
-            log::debug!("Redirecting for unknown cookie");
-            return Redirect::to("/admin/login")
-                .see_other()
-                .respond_to(&request)
-                .map_into_boxed_body();
-        }
-    };
-    log::debug!("Session ID: {session_id}");
-    if db::AdminSession::verify(&db_pool, session_id)
+    if utils::verify_admin_authentication(&session, db_pool)
         .await
-        .unwrap()
+        .unwrap_or_default()
         != true
     {
-        log::debug!("Redirecting for non-match");
         return Redirect::to("/admin/login")
             .see_other()
             .respond_to(&request)
